02f7afc80a5baca34721cc2b25c80c42ee740985
alessandrostorchi92 <storchi.alle@gmail.com>
2023-11-14 01:53:37 +0100

    add cards front-office

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,57 @@
 <script>
 
+import axios from 'axios';
+import ProjectCards from './components/ProjectCards.vue';
+
+
 export default {
 
   components: {
-
+    ProjectCards
   },
 
   data() {
 
     return {
 
-
+      projectsList: [],
+      pagination: {}
 
     };
 
   },
+
+  methods: {
+
+    fetchData() {
+
+      const apiProjects = 'http://127.0.0.1:8000/api/projects';
+
+      // Attraverso axios comunico con l'API per ricevere i dati su Vue.js 
+      axios.get(apiProjects).then((response) => {
+
+        // console.log(response); 
+
+        // Salvo i dati all'interno dell'array projects 
+        this.projectsList = response.data.results.data;
+
+        // Salvo i dati relativi alla pagination
+        delete response.data.results.data;
+        this.pagination = response.data.results;
+
+      })
+
+
+
+    }
+
+  },
+
+  mounted() {
+
+    this.fetchData()
+
+  }
 
 };
 
@@ -22,8 +59,30 @@ export default {
 
 
 <template>
-  Ciao
+  <header></header>
+
+  <main>
+
+    <div class="container my-5">
+
+      <div class="row row-cols-3 gap-5">
+
+        <!-- Quando voglio utilizzare il componente ProjectCards devo passare questa lista collegando le props con il data() -->
+        <!-- Devo mettere "project" e non "projectsList" altrimenti gli ripasso la lista -->
+        <ProjectCards v-for="project in projectsList" :project="project"></ProjectCards>
+
+      </div>
+
+    </div>
+
+  </main>
+
+  <footer></footer>
 </template>
 
 
 <style lang="scss"></style>
+
+
+
+
---
+++ src/components/ProjectCards.vue
@@ -0,0 +1,62 @@
+<script>
+
+export default {
+
+    //Utilizzo il metodo delle props per trasferire i dati dal componente padre App.vue al componente figlio ProjectCard.vue
+    props: {
+
+        project: {
+            type: Object,
+            required: true
+        },
+
+    },
+
+};
+
+</script>
+
+
+<template>
+    <div class="card text-center" style="width: 18rem;">
+
+        <img :src="project.thumb" class="card-img-top" alt="Project Image">
+
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ project.description }}</p>
+        </div>
+
+        <ul class="list-group list-group-flush">
+
+            <li class="list-group-item"><span class="badge text-light" :style="`background-color: rgb(${project.type.colour})`">{{ project.type.name }}</span></li>
+
+            <li class="list-group-item"><span class="badge me-1" v-for="technology in project.technologies" :key="technology.id" :style="`background-color: rgb(${technology.colour})`">{{ technology.name }}</span></li>
+
+            <li class="list-group-item">{{ project.published_date }}</li>
+            <li class="list-group-item">{{ project.language }}</li>
+
+        </ul>
+
+        <div class="card-body">
+            <a href="{{ project.link }}" target="_blank" class="btn btn-primary"><i class="fa-brands fa-github"></i></a>
+        </div>
+
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.card {
+
+    box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+    transition: transform 0.3s ease-out;
+
+
+    &:hover {
+        transform: scale(1.1);
+    }
+
+}
+</style>
